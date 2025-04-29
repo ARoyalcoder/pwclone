c67d51b82fd2a081818946815bdd01ba157b2156
Aditya singh <firstadityasingh@gmail.com>
2025-04-29 16:04:59 +0530

    Normalize line endings using .gitattributes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    host: '0.0.0.0', // 🛑 Add this
-    port: 5173,       // (optional) specify the port if you want
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // increase warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor'
+            }
+            if (id.includes('shadcn') || id.includes('@radix-ui')) {
+              return 'ui-lib'
+            }
+            return 'vendor'
+          }
+
+          if (id.includes('src/pages')) {
+            return 'pages'
+          }
+        },
+      },
+    },
   },
 })
-
